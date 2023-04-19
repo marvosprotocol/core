274479280c515cf5c9551b47ea7a070add53e52c
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-04-20 00:32:22 +0200

    Add fixtures for offers

--- evm/test/utils/fixtures.ts
+++ evm/test/utils/fixtures.ts
@@ -7,7 +7,7 @@ import {
 } from '../../build/types'
 import { ethers, upgrades } from 'hardhat'
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
-import { parseEther } from 'ethers/lib/utils'
+import { arrayify, parseEther } from 'ethers/lib/utils'
 import { prefills } from './prefills'
 const { getContractFactory, getSigners } = ethers
 
@@ -17,6 +17,14 @@ export async function loadBaseTestFixture() {
 
 export async function loadTestWithTokenFixture() {
   return await loadFixture(testWithTokenFixture)
+}
+
+export async function loadOfferWithTokenFixture() {
+  return await loadFixture(offerWithTokenFixture)
+}
+
+export async function loadOfferWithoutTokenFixture() {
+  return await loadFixture(offerWithoutTokenFixture)
 }
 
 async function baseTestFixture() {
@@ -71,7 +79,6 @@ async function testWithTokenFixture() {
     initializer: 'initialize',
   })) as SampleToken
   await sampleToken.deployed()
-  await sampleToken.transfer(alice.address, parseEther('10'))
   sampleToken = sampleToken.connect(alice)
 
   return {
@@ -85,5 +92,87 @@ async function testWithTokenFixture() {
     escrow,
     itroca,
     sampleToken,
+  }
+}
+
+async function offerWithoutTokenFixture() {
+  const {
+    prefills,
+    protocolFees,
+    escrowFeeCommission,
+    maxEscrowFeePercentage,
+    admin,
+    alice,
+    bob,
+    escrow,
+    itroca,
+  } = await loadBaseTestFixture()
+  const offer = prefills.offerPrefill()
+  offer.id = 1
+  offer.creator = alice.address
+  offer.status = 1
+  offer.item.itemData = '0xabcd'
+  offer.item.hasExternalItem = true
+  offer.item.disputeHandler = escrow.address
+  offer.item.disputeHandlerFeeReceiver = escrow.address
+  offer.item.disputeHandlerProof = await escrow.signMessage(
+    arrayify(await itroca.generateHashForOffer(offer)),
+  )
+
+  return {
+    prefills,
+    protocolFees,
+    escrowFeeCommission,
+    maxEscrowFeePercentage,
+    admin,
+    alice,
+    bob,
+    escrow,
+    itroca,
+    offer: Object.freeze(offer),
+  }
+}
+
+async function offerWithTokenFixture() {
+  const {
+    prefills,
+    protocolFees,
+    escrowFeeCommission,
+    maxEscrowFeePercentage,
+    admin,
+    alice,
+    bob,
+    escrow,
+    itroca,
+    sampleToken,
+  } = await loadTestWithTokenFixture()
+  const offer = prefills.offerPrefill()
+  offer.id = 1
+  offer.creator = alice.address
+  offer.status = 1
+  offer.item.itemData = '0xabcd'
+  offer.token = sampleToken.address
+  offer.maxAmount = parseEther('1')
+  offer.minAmount = parseEther('0.1')
+  offer.totalAmount = parseEther('10')
+  offer.availableAmount = parseEther('10')
+  offer.item.disputeHandler = escrow.address
+  offer.item.disputeHandlerFeeReceiver = escrow.address
+  offer.item.disputeHandlerProof = await escrow.signMessage(
+    arrayify(await itroca.generateHashForOffer(offer)),
+  )
+
+  return {
+    prefills,
+    protocolFees,
+    escrowFeeCommission,
+    maxEscrowFeePercentage,
+    admin,
+    alice,
+    bob,
+    escrow,
+    itroca,
+    sampleToken,
+    offer: Object.freeze(offer),
   }
 }
