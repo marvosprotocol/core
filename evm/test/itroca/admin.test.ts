7decd1577df693c2ba575e3fcafe78cc92fe768f
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-04-19 23:13:05 +0200

    Switch to fixtures

--- evm/test/itroca/admin.test.ts
+++ evm/test/itroca/admin.test.ts
@@ -1,53 +1,21 @@
-﻿/* eslint-disable @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access */
+﻿/* eslint-disable @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment */
 import { expect } from 'chai'
-import { ethers, upgrades } from 'hardhat'
-import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-waffle'
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
-import { ITroca, ITroca__factory } from '../../build/types'
 import { randomAddressString } from 'hardhat/internal/hardhat-network/provider/utils/random'
-import { getAddress } from 'ethers/lib/utils'
-const { getContractFactory, getSigners } = ethers
+import { loadBaseTestFixture, loadTestWithTokenFixture } from '../utils'
 
 describe('ITroca', () => {
-  let itroca: ITroca
-  let admin: SignerWithAddress
-  let alice: SignerWithAddress
-  let signers: SignerWithAddress[]
-
-  const protocolFees = 25 // 0.2%
-  const escrowFeeCommission = 500 // 5%
-  const maxEscrowFeePercentage = 2000
-
-  before(async () => {
-    signers = await getSigners()
-    admin = signers[0]
-    alice = signers[1]
-  })
-
-  beforeEach(async () => {
-    const itrocaFactory = (await getContractFactory('ITroca', admin)) as ITroca__factory
-    itroca = (await upgrades.deployProxy(
-      itrocaFactory,
-      [protocolFees, escrowFeeCommission, maxEscrowFeePercentage],
-      {
-        initializer: 'initialize',
-      },
-    )) as ITroca
-    await itroca.deployed()
-    itroca = itroca.connect(alice)
-  })
-
   describe('ITroca Admin Functions', () => {
     describe('setProtocolFeePercentage', () => {
       describe('validations', () => {
         it('should revert when called by any address', async () => {
+          const { itroca, alice } = await loadBaseTestFixture()
           await expect(itroca.connect(alice).setProtocolFeePercentage(30)).to.be.reverted
         })
       })
 
       describe('effects', () => {
         it('should set the protocolFeePercentage field to a new value', async () => {
+          const { itroca, admin } = await loadBaseTestFixture()
           await expect(itroca.connect(admin).setProtocolFeePercentage(30))
             .to.emit(itroca, 'ProtocolFeePercentageUpdated')
             .withArgs(30)
@@ -58,6 +26,7 @@ describe('ITroca', () => {
     describe('setDisputeHandlerFeePercentageCommission', () => {
       describe('validations', () => {
         it('should revert when called by any address', async () => {
+          const { itroca, alice } = await loadBaseTestFixture()
           await expect(
             itroca.connect(alice).setDisputeHandlerFeePercentageCommission(250),
           ).to.be.reverted
@@ -66,6 +35,7 @@ describe('ITroca', () => {
 
       describe('effects', () => {
         it('should update the commission on dispute handler fees', async () => {
+          const { itroca, admin } = await loadBaseTestFixture()
           await expect(
             itroca.connect(admin).setDisputeHandlerFeePercentageCommission(250),
           )
@@ -78,6 +48,7 @@ describe('ITroca', () => {
     describe('setMaxDisputeHandlerFeePercentage', () => {
       describe('validations', () => {
         it('should revert when called by any address', async () => {
+          const { itroca, alice } = await loadBaseTestFixture()
           await expect(itroca.connect(alice).setMaxDisputeHandlerFeePercentage(1000)).to
             .be.reverted
         })
@@ -85,6 +56,7 @@ describe('ITroca', () => {
 
       describe('effects', () => {
         it('should update the maximum dispute handler fee', async () => {
+          const { itroca, admin } = await loadBaseTestFixture()
           await expect(itroca.connect(admin).setMaxDisputeHandlerFeePercentage(1000))
             .to.emit(itroca, 'MaxDisputeHandlerFeePercentageUpdated')
             .withArgs(1000)
@@ -95,6 +67,7 @@ describe('ITroca', () => {
     describe('setTokenBlacklisted', () => {
       describe('validations', () => {
         it('should revert when called by any address', async () => {
+          const { itroca, alice } = await loadBaseTestFixture()
           await expect(
             itroca.connect(alice).setTokenBlacklisted(randomAddressString(), true),
           ).to.be.reverted
@@ -103,10 +76,12 @@ describe('ITroca', () => {
 
       describe('effects', () => {
         it('should update a token blacklist status', async () => {
-          const token = getAddress(randomAddressString())
-          await expect(itroca.connect(admin).setTokenBlacklisted(token, true))
+          const { itroca, admin, sampleToken } = await loadTestWithTokenFixture()
+          await expect(
+            itroca.connect(admin).setTokenBlacklisted(sampleToken.address, true),
+          )
             .to.emit(itroca, 'TokenBlacklistStatusUpdated')
-            .withArgs(token, admin.address, true)
+            .withArgs(sampleToken.address, admin.address, true)
         })
       })
     })
@@ -114,12 +89,14 @@ describe('ITroca', () => {
     describe('pause', () => {
       describe('validations', () => {
         it('should revert when called by any address', async () => {
+          const { itroca, alice } = await loadBaseTestFixture()
           await expect(itroca.connect(alice).pause()).to.be.reverted
         })
       })
 
       describe('effects', () => {
         it('should pause the contract', async () => {
+          const { itroca, admin } = await loadBaseTestFixture()
           await expect(itroca.connect(admin).pause())
             .to.emit(itroca, 'Paused')
             .withArgs(admin.address)
@@ -130,6 +107,7 @@ describe('ITroca', () => {
     describe('unpause', () => {
       describe('validations', () => {
         it('should revert when called by any address', async () => {
+          const { itroca, admin, alice } = await loadBaseTestFixture()
           await itroca.connect(admin).pause()
           await expect(itroca.connect(alice).unpause()).to.be.reverted
         })
@@ -137,6 +115,7 @@ describe('ITroca', () => {
 
       describe('effects', () => {
         it('should unpause the contract', async () => {
+          const { itroca, admin } = await loadBaseTestFixture()
           await itroca.connect(admin).pause()
           await expect(itroca.connect(admin).unpause())
             .to.emit(itroca, 'Unpaused')
