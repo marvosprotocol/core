b65c0a8a12690197e9b62c3b995e72fb63a9386b
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-04-24 12:18:36 +0200

    Add external data spec

---
+++ evm/external-data-spec/SPEC.md
@@ -0,0 +1,16 @@
+﻿# Marvos External Data Spec
+
+The Marvos core contract powers the exchange process. However, data such as offer token pricing is not stored on chain.
+This allows maximum flexibility in order processing, allowing for unique pricing structures, negotiations, etc. As a
+result, clients have a responsibility to ensure data visibility so that traders can have a seamless experience.
+
+The EDS details the specification for the data fields. We have chosen to define the EDS using TypeScript type
+definitions because of the simplicity and expressiveness of TypeScript's type system. The type definitions can be found
+[here](https://github.com/marvosprotocol/core/tree/evm/external-data-spec/spec.ts).
+
+### Non-Fungible External Item Registry 
+
+| NFSI Code  | ID Format                                      | Description          |
+|------------|------------------------------------------------|----------------------|
+| nft        | SHA256(nfei:chainCode/collectionAddress/nftId) | NFTs                 |
+| sm-account | SHA256(nfei:shortestSMHostname/username)       | Social Media Account |
---
+++ evm/external-data-spec/spec.ts
@@ -0,0 +1,63 @@
+﻿interface ExternalData {
+  // additional text information provided
+  note: string
+  supply: Items
+  demand: Items
+}
+
+type Items = null | ExternalItem | MultipleItemCombination
+
+type ExternalItem = FungibleExternalItem | NonFungibleExternalItem
+
+type FungibleExternalItem = FiatExternalItem | CryptoExternalItem
+
+interface MultipleItemCombination {
+  rule: 'and' | 'or'
+  values: Array<ExternalItem | MultipleItemCombination>
+}
+
+interface NonFungibleExternalItem {
+  // can be anything. but an NFEI directory is being maintained to
+  // ensure consistency for common NFEI types
+  // clients can support additional types
+  // clients should use types from the NFEI directory to enable seamless interop
+  type: string
+
+  // an id that uniquely identifies the NFEI
+  id: string
+
+  properties: Record<string, NFEIProperty>
+}
+
+interface FiatExternalItem extends BaseFungibleExternalItem {
+  type: 'fiat'
+}
+
+interface CryptoExternalItem extends BaseFungibleExternalItem {
+  type: 'crypto'
+
+  // network code from registry
+  network: string
+
+  // token address - when null, represents network native cryptocurrency
+  address?: string
+}
+
+interface BaseFungibleExternalItem {
+  // currency short code. eg: USD or ETH
+  currency: string
+
+  // amount in hex in smallest unit of currency. Like cents for USD or wei for ETH.
+  value: string
+}
+
+// complex properties should be encoded as JSON
+interface NFEIProperty {
+  type: 'utf8' | 'integer' | 'decimal' | 'boolean'
+
+  // data is the utf8 string for 'utf8'
+  // 'true' or 'false' for 'boolean'
+  // the integer encoded as hex for 'integer'
+  // the decimal encoded as hex with the decimal point in place for 'decimal'
+  data: string
+}
